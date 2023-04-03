96383a3149bafd649c21dc274bf5dabb8cdc0beb
mayaahmedabdelsameaa <mayarahmed8624@gmail.com>
2023-04-03 22:30:14 +0200

    adding my part

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/models/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
+  employees: Employee[] = [];
+  constructor(
+    private employeeServices: EmployeeService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.employeeServices.getAll().subscribe(
+      (a: any) => {
+        this.employees = a;
+        console.log(this.employees);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  UpdateEmp(id:number){
+    sessionStorage.setItem('empid', JSON.stringify(id));
+    this.router.navigate(['/employee/edit']);
+  }
 
+  Delete(id: number) {
+    if (confirm('Are you sure, you want to delete it?')) {
+      this.employeeServices.deleteById(id).subscribe((a) => {
+        this.employees = this.employees.filter((emp) => emp._id != id);
+      });
+    }
+  }
 }
--- src/app/prescription/prescription-details/prescription-details.component.ts
+++ src/app/prescription/prescription-details/prescription-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PrescriptionService } from 'src/app/services/prescription.service';
 
 @Component({
   selector: 'app-prescription-details',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./prescription-details.component.css']
 })
 export class PrescriptionDetailsComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private prescriptionService: PrescriptionService
+  ) {}
 
+  id!: any;
+  data: any;
+  medecines!: any;
+  error = false;
+
+  ngOnInit(): void {
+    this.id = this.route.params.subscribe((prescription) => {
+      this.prescriptionService.getById(3).subscribe(
+        (b) => {
+          this.data = b;
+          console.log(this.data)
+        },
+        (error) => {
+          this.error = true;
+        }
+      );
+    });
+  }
 }
